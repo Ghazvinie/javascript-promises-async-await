484c4db2abdd773ec25616313132c67b164afed9
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-02-14 10:12:49 +0000

    added code as per instructions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+let movies = require ('./src/data/movies.json');
+import fetchWithTimeout from './services';
+
 export function fetchMovies(){
+
+    const resolveFunction = () => movies;
+
+    return fetchWithTimeout(1000).then(resolveFunction);
 
 }
 
+const moviePromise = fetchMovies().then(console.log(resolve(results)));
